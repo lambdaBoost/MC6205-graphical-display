6c05389922c8be79700838b53f48ad7578a378d3
Hall <Alexander.Hall@sse.com>
2022-10-06 22:09:09 +0100

    first pass image display written, along with on-machine image conversion

--- full-graphical/code/image-processing/convert_image.py
+++ full-graphical/code/image-processing/convert_image.py
@@ -7,9 +7,32 @@ IMAGE_FILE = "../imgs/monochrome_test_card_scaled.png"
 
 img_in = cv2.imread(IMAGE_FILE)
 img_out = cv2.rotate(img_in, cv2.ROTATE_90_CLOCKWISE)
+img_out = cv2.flip(img_out,1)
 img_out = (img_out*(1/255)).astype(int)
 img_out = img_out[:,:,0]
 img_out = img_out.tolist()
 
+#convert to list of ints
+def binary_list_to_int(lst):
+    
+    out = int(bin(int(''.join(map(str, lst)), 2) << 1),2)
+    return out
+
+def reverse_bits(lst):
+    """
+flip bits (used for last 4 anodes
+    """
+    
+    out = [(i*-1) + 1 for i in lst]
+    return out
+
+#first split each row into 32 bit words
+#repeat first word 8 times to make it 32 bits
+display_image = [[ reverse_bits(8*row[99:95:-1]), row[95:63:-1], row[63:31:-1],row[31::-1]] for row in img_out]
+
+#then to int
+display_image = [[binary_list_to_int(word) for word in row] for row in display_image]
+
+
 with open('../imgs/test_card_json.json', 'w') as f:
-    json.dump(img_out,f)
+    json.dump(display_image,f)
--- full-graphical/code/main.py
+++ full-graphical/code/main.py
@@ -1,15 +1,20 @@
+#TODO:
+#increase group count from 8 to 10 when new cathodes are connected
+
 from machine import Pin, SPI
 import rp2
 from time import sleep, sleep_us, sleep_us
 import json
 import anode_write
 
+from anode_write import anode_order
+
 
 import machine
 
 
 CATHODE_PULSE_WIDTH = 1
-CATHODE_HOLD_TIME = 20
+CATHODE_HOLD_TIME = 100
 BLANKING_INTERVAL = 1
 
 group_cathode_clk = Pin(6, Pin.OUT)
@@ -19,58 +24,7 @@ individual_cathode_rst = Pin(3, Pin.OUT)
 
 #test image for now
 display_image = anode_write.read_test_file()
-    
 
-@rp2.asm_pio(
-    set_init=rp2.PIO.OUT_LOW,
-    out_init=rp2.PIO.OUT_LOW,
-    sideset_init=rp2.PIO.OUT_LOW,
-    autopull=True
-)
-def matrix_row():
-    """
-    Send 32 bits to the DATA pin and latch
-    """
-    pull()
-    label('reset_x1')
-    set(x, 31)                       # loop counter, loop 16 times
-    label('bitloop1')
-    out(pins, 1)       .side(1)      # {1} mov 1bit to DATA pin
-                                     #     and set CLOCK high
-    jmp(x_dec, 'delay1')              # {2} jump to 'delay'
-    
-    pull()
-    set(x, 31)                       # loop counter, loop 16 times
-    label('bitloop2')
-    out(pins, 1)       .side(1)      # {1} mov 1bit to DATA pin
-                                     #     and set CLOCK high
-    jmp(x_dec, 'delay2')              # {2} jump to 'delay'
-    
-    pull()
-    set(x, 31)                       # loop counter, loop 16 times
-    label('bitloop3')
-    out(pins, 1)       .side(1)      # {1} mov 1bit to DATA pin
-                                     #     and set CLOCK high
-    jmp(x_dec, 'delay3')              # {2} jump to 'delay'
-    
-    
-    set(pins, 1)                     # {3} set LATCH high
-    set(x, 31)         .side(0)   [2]   # {4} reset counter
-    set(pins, 0)                     # {5} set LATCH low
-    jmp('reset_x1')                   # {6} restart loop
-
-    
-    label('delay1')
-    nop()                            # {3}
-    jmp('bitloop1')     .side(0)  [2] # {4-6}    
-    
-    label('delay2')
-    nop()                            # {3}
-    jmp('bitloop2')     .side(0)  [2] # {4-6}
-    
-    label('delay3')
-    nop()                            # {3}
-    jmp('bitloop3')     .side(0)  [2] # {4-6}    
 
 
 @rp2.asm_pio(
@@ -98,7 +52,7 @@ def matrix_row_2():
     jmp(y_dec,'reset_x')
     
     set(pins, 1)                     # {3} set LATCH high
-    set(y, 2)         .side(0)   [2]   # {4} reset counter
+    set(y, 3)         .side(0)   [2]   # {4} reset counter
     set(pins, 0)                     # {5} set LATCH low
     nop()								#delay for blanking operation
     nop()
@@ -118,95 +72,87 @@ def matrix_row_2():
     
 
 
-
-"""
-def latch_row():
-    wrap_target()
-    set(pins, 1)  [6]                   # {3} set LATCH high
-    set(pins, 0)                     # {5} set LATCH low
   
-    
-"""   
-sm0 = rp2.StateMachine(0, matrix_row_2, freq=3000000,
+sm0 = rp2.StateMachine(0, matrix_row_2, freq=5000000,
                       sideset_base=Pin(2),    # CLOCK pin
                       set_base=Pin(1),        # LATCH pin
                       out_base=Pin(0),        # DATA pin
                      )
 
-"""
-sm1 = rp2.StateMachine(1, latch_row, freq=100000,
-                      sideset_base=Pin(2),    # CLOCK pin
-                      set_base=Pin(1),        # LATCH pin
-                      out_base=Pin(0),        # DATA pin
-                     )
-                     
-"""
-
-"""
-sm2 = rp2.StateMachine(2, latch_row, freq=100000,
-                      sideset_base=Pin(2),    # CLOCK pin
-                      set_base=Pin(1),        # LATCH pin
-                      out_base=Pin(0),        # DATA pin
-                     )
-"""
 
 
 sm0.active(1)
-#sm0.put(0b11111111111111111111111111111111)
-sm0.exec("set(y, 2)")
+sm0.exec("set(y, 3)")
+
+
+group_cathode_rst.value(1)
+sleep_us(CATHODE_PULSE_WIDTH)
+group_cathode_rst.value(0)
+
+individual_cathode_rst.value(1)
+sleep_us(CATHODE_PULSE_WIDTH)
+individual_cathode_rst.value(0)
+
 
 while True:
     
+    anode_count = 0
     #sm0.exec("set(y, 2)")
     
+    #TODO - only counts 8 groups for now. Remember to increase when missing cathode are connected
     for i in range(10):
-        group_cathode_clk.value(1)
-        sleep_us(CATHODE_PULSE_WIDTH)
-        group_cathode_clk.value(0)
+        if i>0:
+            group_cathode_clk.value(1)
+            sleep_us(CATHODE_PULSE_WIDTH)
+            group_cathode_clk.value(0)
         #sleep_us(CATHODE_HOLD_TIME)
         
         
         
         for j in range(10):
+            
+            anode_num = anode_order[anode_count]
+            row = display_image[anode_num]
+            
+            if j > 0:
+                individual_cathode_clk.value(1)
+                sleep_us(CATHODE_PULSE_WIDTH)
+                individual_cathode_clk.value(0)
 
-            individual_cathode_clk.value(1)
-            sleep_us(CATHODE_PULSE_WIDTH)
-            individual_cathode_clk.value(0)
-            #value = 2863311530 #32 bit decimal for alternating 1s and 0s
-            
-            #sm.restart()
-            
-            if j % 2 ==0:
-                #value = 2863311530 #10 pattern
-                value = 0b01010101010101010101010101010101
-            else:
-                value = 0b10101010101010101010101010101010
-                #value = 1431655765 #alternating 10 pattern
-            #value = 0b000000000000000000000000000000
-            
- 
 
-            #data
-            sm0.put(value)
-            sm0.put(0b000000000000000000000000000000)
-            sm0.put(value)
+            sm0.put(row[0])          
+            sm0.put(row[1])
+            sm0.put(row[2])
+            sm0.put(row[3])
+
+
+
             sleep_us(CATHODE_HOLD_TIME)
             
             #blanking - do before data to shorten delay
-            sm0.put(0b000000000000000000000000000000)
-            sm0.put(0b000000000000000000000000000000)
-            sm0.put(0b000000000000000000000000000000)
+            #sm0.put(0b000000000000000000000000000000)
+            #sm0.put(0b000000000000000000000000000000)
+            #sm0.put(0b000000000000000000000000000000)
+            #sm0.put(0b000000000000000000000000000000)
             #sleep_us(BLANKING_INTERVAL)
+            
+            anode_count = anode_count + 1
             
 
             
 
             #sm0.active(0)
             
-
-            
+        individual_cathode_rst.value(1)
+        sleep_us(CATHODE_PULSE_WIDTH)
+        individual_cathode_rst.value(0)
+        
+    group_cathode_rst.value(1)
+    sleep_us(CATHODE_PULSE_WIDTH)
+    group_cathode_rst.value(0)
 
             
 
 
         
+ 
