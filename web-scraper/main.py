c0caeba76e847fa29433e8a83799feb54ab04a5a
Alex Hall <analytics@alex-hall.co.uk>
2022-10-08 12:04:55 +0100

    added image hosting API

--- web-scraper/main.py
+++ web-scraper/main.py
@@ -9,9 +9,16 @@ import requests
 from bs4 import BeautifulSoup
 import re
 from fastapi import FastAPI
+from fastapi.encoders import jsonable_encoder
+from fastapi.responses import JSONResponse
 from pydantic import BaseModel
 import os
 import datetime
+
+import cv2
+import numpy as np
+import json
+
 
 
 URL = "https://www.oryxspioenkop.com/2022/02/attack-on-europe-documenting-equipment.html"
@@ -54,3 +61,44 @@ async def get_time():
 
 
 
+@app.get("/test_image/")
+async def get_test_image():
+    """
+    processes and returns the test image for display in json format
+    """
+    
+    IMAGE_FILE = "./imgs/monochrome_test_card_scaled.png"
+
+    img_in = cv2.imread(IMAGE_FILE)
+    
+
+    
+    img_out = cv2.rotate(img_in, cv2.ROTATE_90_CLOCKWISE)
+    img_out = cv2.flip(img_out,1)
+    img_out = (img_out*(1/255)).astype(int)
+    img_out = img_out[:,:,0]
+    img_out = img_out.tolist()
+
+    #convert to list of ints
+    def binary_list_to_int(lst):
+    
+        out = int(bin(int(''.join(map(str, lst)), 2) << 1),2)
+        return out
+
+    def reverse_bits(lst):
+        """
+        flip bits (used for last 4 anodes
+        """
+    
+        out = [(i*-1) + 1 for i in lst]
+        return out
+
+    #first split each row into 32 bit words
+    #repeat first word 8 times to make it 32 bits
+    display_image = [[ reverse_bits(8*row[99:95:-1]), row[95:63:-1], row[63:31:-1],row[31::-1]] for row in img_out]
+
+    #then to int
+    display_image = [[binary_list_to_int(word) for word in row] for row in display_image]
+
+    json_compatible_item_data = jsonable_encoder(display_image)
+    return JSONResponse(content=json_compatible_item_data)
